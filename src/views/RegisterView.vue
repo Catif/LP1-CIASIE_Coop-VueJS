405d779bc3f8d7736203ed780447b355f2e91c16
Catif <bradley.barbier@outlook.fr>
2022-12-03 23:40:40 +0100

    Auto import + Create components icons

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,4 @@
 <script setup>
-import Alert from '@/components/Alert.vue'
-
 import {useRouter} from 'vue-router';
 const router = useRouter();
 
@@ -14,6 +12,7 @@ const error = ref('');
 
 let register = () => {
   error.value = '';
+
   api.post('members',{body: member}).then(data => {
     if (data.message){
       error.value = data.message
@@ -27,16 +26,10 @@ let register = () => {
 
 
 
-let symboleEyesPassword = ref('--')
 let showPassword = ref(false);
 
 let togglePassword = () => {
   showPassword.value = !showPassword.value
-  if (showPassword.value){
-    symboleEyesPassword.value = 'O'
-  } else {
-    symboleEyesPassword.value = '--'
-  }
 }
 
 </script>
@@ -61,7 +54,10 @@ let togglePassword = () => {
         <div class="password">
           <input v-if="!showPassword" type="password" v-model="member.password" require>
           <input v-else type="text" v-model="member.password" require>
-          <button type="button" @click="togglePassword">{{symboleEyesPassword}}</button>
+          <button type="button" @click="togglePassword">
+              <ClosedEye v-if="!showPassword" />
+              <OpenEye v-else />
+          </button>
         </div>
       </div>
       <div class="list-button">
@@ -74,17 +70,33 @@ let togglePassword = () => {
 
 <style lang="scss" scoped>
   .password {
-    display: flex;
+    position: relative;
+
     input {
-      flex-grow: 1;
-      border-radius: 5px 0px 0px 5px;
+      border-radius: 5px;
+      width: 100%;
     }
     button {
+      height: 100%;
+      position: absolute;
       border: none;
-      background-color: hsl(212, 90%, 20%);
+      background-color: transparent;
+      right: 0;
+      top: 0;
       color: #E5E5E5;
       width: 30px;
       border-radius: 0px 5px 5px 0px;
+      cursor: pointer;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      svg{
+        width: 18px;
+        height: 18px;
+        color: hsl(212, 90%, 20%);
+      }
     }
   }
 </style>
